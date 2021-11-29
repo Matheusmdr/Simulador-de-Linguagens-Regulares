a498f1df697d61cbae74a9b29ef3a12060d19883
matheusmdr <matheus.m.rocha@unesp.br>
2021-11-28 22:45:13 -0300

    build: add react-router-dom to React App

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import "../App.scss"
+
+export function Home() {
+
+    return(
+        <div className="App">
+             <h1 className="App-header">Hello World</h1>
+        </div>
+    )
+    
+  }
